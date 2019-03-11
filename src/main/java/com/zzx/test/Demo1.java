e45ba6a7e39f8d37d6b63963a5fde05525100431
gengu <genxiaogu@126.com>
2019-03-11 17:44:29 +0800

    19-3-11

---
+++ src/main/java/com/zzx/test/Demo1.java
@@ -0,0 +1,13 @@
+package com.zzx.test;
+
+public class Demo1 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		Class.forName("com.mysql.jdbc.Driver");
+		System.out.println("初始化驱动成功");
+		System.out.println("Hello World!");
+	}
+
+}
